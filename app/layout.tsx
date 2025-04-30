f07ea6bad43588166e773c0811ea76f296510b2a
Serge Selenou <selserge040404@gmail.com>
2025-04-29 20:23:06 -0400

    getting rid of three

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/next';
-import ThreeBackground from "@/components/ThreeBackground";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
